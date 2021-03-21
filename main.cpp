701da0c5b95ab3ad766ff8f78614683bc8c5bfce
Dhruv Arora <dhruvarora.arora999@gmail.com>
2021-03-21 19:00:50 +0530

    Initial Release.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  QualityIsDown
+//
+//  Created by Apple on 13/03/21.
+//
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+float findEuclideanDistance(int a, int b){
+    
+    float ans = a*a + b*b;
+    return pow(ans, 0.5);
+    
+}
+int main() {
+
+    cout << "This Project aims to find the Sqaure root of sum of sqaures of integers;"<<endl;
+    cout<<"Probably the above line also may contain en error :("<<endl;
+    int a, b;
+    cin>>a>>b;
+    cout<<findEuclideanDistance(a, b)<<endl;
+    return 0;
+}
